1bb49008cffced023628b33b699d92b6219b65a1
Sujeet Kumar <iamsujeetsarkar@gmail.com>
2020-01-09 23:55:43 +0530

    Parent to Child: Sharing Data via Input
    Passing Data from Parent to Child Component

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html'
+})
+export class ChildComponent implements OnInit {
+  @Input() childMessageVariable: string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
