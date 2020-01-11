e8a1b190e5a2c79e84887b6ceaf06cd8b537139f
Sujeet Kumar <iamsujeetsarkar@gmail.com>
2020-01-12 00:49:05 +0530

    Another use of ViewChild to get the reference of element

--- src/app/parent2/parent2.component.ts
+++ src/app/parent2/parent2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, AfterViewInit, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, AfterViewChecked, ElementRef } from '@angular/core';
 import { Child2Component } from '../child2/child2.component';
 
 @Component({
@@ -7,6 +7,7 @@ import { Child2Component } from '../child2/child2.component';
 })
 export class Parent2Component implements OnInit, AfterViewInit, AfterViewChecked {
   @ViewChild(Child2Component, {static: false}) ChildComponentRef: Child2Component;
+  @ViewChild('Parent2', {static: false}) ParentEleRef: ElementRef;
 
   message: string;
   messageAfterViewChecked: string;
@@ -17,6 +18,7 @@ export class Parent2Component implements OnInit, AfterViewInit, AfterViewChecked
 
   ngAfterViewInit() {
     this.message = this.ChildComponentRef.message;
+    console.log(this.ParentEleRef.nativeElement);
   }
   ngAfterViewChecked() {
     this.messageAfterViewChecked = this.ChildComponentRef.message;
